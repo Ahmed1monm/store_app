ae2941a19850c4d7cd281b99a3264d76d56eb978
Ahmed1monm <60390230+Ahmed1monm@users.noreply.github.com>
2022-02-13 17:05:18 +0200

    Creating home page
    creating the builder of grid view

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -7,22 +7,77 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.white,
-      elevation: 0,
-      title: const Text(
-        'New Trend',
-        style: TextStyle(color: Colors.black),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          'New Trend',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(FontAwesomeIcons.cartPlus),
+            color: Colors.black,
+          )
+        ],
       ),
-      centerTitle: true,
-      actions: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(FontAwesomeIcons.cartPlus),
-          color: Colors.black,
-        )
-      ],
-    ));
+      body: GridView.builder(
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index) => gridBuilder(
+            description: 'Description',
+            title: 'Ttle',
+            context: context,
+            imageUrl:
+                'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg'),
+        itemCount: 10,
+      ),
+    );
+  }
+
+  Widget gridBuilder(
+      {required String imageUrl,
+      required BuildContext context,
+      required String title,
+      required String description}) {
+    double cardHeight = MediaQuery.of(context).size.height * 0.2;
+    return Container(
+      height: cardHeight,
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image(
+              image: NetworkImage(imageUrl),
+              fit: BoxFit.cover,
+              height: cardHeight * 0.8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(title),
+                    Text(description),
+                  ],
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      FontAwesomeIcons.heart,
+                      color: Colors.red,
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
